8aeda860e9b4876afd1b2e48f55edd641927a60c
Wilhelm <>
2021-08-21 22:44:46 +0200

    Fix string

--- pagePerformance.js
+++ pagePerformance.js
@@ -1,3 +1,3 @@
 const performanceQualcosa = performance.now();
 
-document.getElementById("myPerformance").innerHTML = "Browser performance (refresh to check) : " + performanceQualcosa + " milliseconds";
+document.getElementById("myPerformance").innerHTML = `Browser performance (refresh to check) : ${performanceQualcosa} milliseconds`;
